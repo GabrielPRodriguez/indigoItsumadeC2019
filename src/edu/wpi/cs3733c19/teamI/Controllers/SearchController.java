de00461370f03afb10ecbdedd081387e5b214522
Gabriel Rodriguez <grodriguez@wpi.edu>
2019-02-02 15:43:59 -0500

    Added all the fields from the fxml search page into SearchController

--- src/edu/wpi/cs3733c19/teamI/Controllers/SearchController.java
+++ src/edu/wpi/cs3733c19/teamI/Controllers/SearchController.java
@@ -5,6 +5,7 @@ import edu.wpi.cs3733c19.teamI.Entities.SearchResults;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.RadioButton;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
@@ -15,11 +16,75 @@ import java.util.LinkedList;
 public class SearchController {
     private SearchResults Results = new SearchResults();
 
+    // Search Button
     @FXML
     Button performSearch;
 
+    // REP ID NO
+    @FXML
+    TextField repIDNO;
+
+    // Plant Registry/Basic Permit/ Brewer's NO
+    @FXML
+    TextField plantRegistry;
+
+    // #3 Domestic or Imported (they work as two separe entities)
+    @FXML
+    RadioButton domestic;
+    @FXML
+    RadioButton imported;
+
+    // Serial Number
+    @FXML
+    TextField serialNumber;
+
+    // Type of Product
+    @FXML
+    RadioButton wine;
+    @FXML
+    RadioButton beer;
+    @FXML
+    RadioButton liqour;
+
+    // Brand Name
     @FXML
     TextField brandNameField;
+
+    // fancifulName
+    @FXML
+    TextField fancifulName;
+
+    // Vintage (wine only)
+    @FXML
+    TextField vintage;
+
+    // PH Level (Wine only)
+    @FXML
+    TextField phLevel;
+
+    // Alcohol Percentage
+    @FXML
+    TextField alcoholPercentage;
+
+    // Formula
+    @FXML
+    TextField formula;
+
+    // Grape Variental(s) (wine only)
+    @FXML
+    TextField grapeVarietal;
+
+    // Wine Appellation (if on label)
+    @FXML
+    TextField wineAppellation;
+
+    // Phone Number
+    @FXML
+    TextField phoneNumber;
+
+    // Email Address
+    @FXML
+    TextField emailAddress ;
 
     @FXML
     protected void fillSearchParam()

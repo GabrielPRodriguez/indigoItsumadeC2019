d19a54c905293e7491ce3cdbe56c1989ed57855d
Gabriel Rodriguez <grodriguez@wpi.edu>
2019-02-05 03:27:17 -0500

    Working on improving the looks and functionality of the UI

---
+++ src/edu/wpi/cs3733c19/teamI/Controllers/HomepageNewController.java
@@ -0,0 +1,6 @@
+package edu.wpi.cs3733c19.teamI.Controllers;
+
+// https://www.youtube.com/watch?v=H6agC5oZ7xE
+
+public class HomepageNewController {
+}
---
+++ src/edu/wpi/cs3733c19/teamI/Controllers/TittleBarNewController.java
@@ -0,0 +1,13 @@
+package edu.wpi.cs3733c19.teamI.Controllers;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class TittleBarNewController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}

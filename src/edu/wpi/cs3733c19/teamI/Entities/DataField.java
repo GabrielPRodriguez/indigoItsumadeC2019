14b4dbec417e2b70f790db9eba97eb907e8d7447
Carkin <ewcarkin@wpi.edu>
2019-02-07 13:21:29 -0500

    commented datafield class

--- src/edu/wpi/cs3733c19/teamI/Entities/DataField.java
+++ src/edu/wpi/cs3733c19/teamI/Entities/DataField.java
@@ -1,11 +1,18 @@
 package edu.wpi.cs3733c19.teamI.Entities;
 
+/*
+This class is used to store search parameters. The DB can have types of String, int or double. In order to be able
+to group all search parameters into a single structure, we needed a way of representing multiple data types in one class.
+Data field has 2 attributes, one of type string to hold what parameter we are storing. Another of "type" that contains
+the value of the parameter. Typ can be ust about anything, and allows us to reference string, double and in in our
+search hash map
+ */
 public class DataField<type> {
 
-    private type Value;
-    private String field;
+    private type Value; // name of parameter
+    private String field; //value of parameter
 
-    //setter
+    //setters
     protected void setValue(type Val)
     {
         this.Value = Val;
@@ -16,7 +23,7 @@ public class DataField<type> {
         this.field = Val;
     }
 
-    //getter
+    //getters
     public type getValue()
     {
         return(this.Value);
@@ -27,6 +34,8 @@ public class DataField<type> {
         return(this.field);
     }
 
+    //this "getter" will return a string rep of whatever data type was stored. This is useful for displaying results
+    //TODO does this need a try-catch in case we cannot convert to string?
     String getStringVal(){
      return(Value.toString());
     }

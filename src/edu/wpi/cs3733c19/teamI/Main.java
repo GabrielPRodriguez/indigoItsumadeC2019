4d34e773c30d5cf9427e18ccdf6f9b95f9d9ade4
Fareya Ikram <fsikram@wpi.edu>
2019-02-07 14:51:45 -0500

    Basic file refactoring, renamed SearchV2 to Searchpage.fxml and deleted Controller.java

--- src/edu/wpi/cs3733c19/teamI/Main.java
+++ src/edu/wpi/cs3733c19/teamI/Main.java
@@ -19,7 +19,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
 
-        FXMLLoader firstPaneLoader = new FXMLLoader(getClass().getResource("Boundaries/SearchV2.fxml"));
+        FXMLLoader firstPaneLoader = new FXMLLoader(getClass().getResource("Boundaries/SearchPage.fxml"));
         Parent firstPane = firstPaneLoader.load();
         Scene firstScene = new Scene(firstPane, 1289, 918);
 

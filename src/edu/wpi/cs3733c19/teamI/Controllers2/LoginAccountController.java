471172d87ffb8ab54d91259a85105d13a4bdbe5f
Rasheeda Samih <rsamih@wpi.edu>
2019-02-10 22:05:56 -0500

    Merge remote-tracking branches 'remotes/origin/Grant_Home_Pages' and 'remotes/origin/new_db_plus_form' into ui_design
    # Conflicts:
    #	src/edu/wpi/cs3733c19/teamI/Main.java

--- src/edu/wpi/cs3733c19/teamI/Controllers2/LoginAccountController.java
+++ src/edu/wpi/cs3733c19/teamI/Controllers2/LoginAccountController.java
@@ -1,4 +1,25 @@
 package edu.wpi.cs3733c19.teamI.Controllers2;
 
-public class LoginAccountController {
+import com.jfoenix.controls.JFXTabPane;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public class LoginAccountController implements Initializable {
+
+    @FXML
+    JFXTabPane primaryPane;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        primaryPane.widthProperty().addListener((observable, oldValue, newValue) ->
+        {
+            primaryPane.setTabMinWidth(primaryPane.getWidth() / 2);
+            primaryPane.setTabMaxWidth(primaryPane.getWidth() / 2);
+        });
+    }
 }

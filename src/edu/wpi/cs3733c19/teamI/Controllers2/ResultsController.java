e736d686b6c873be563e67b94183214763dd6cf4
Rachael <resallie@wpi.edu>
2019-02-12 19:56:50 -0500

    Merge branch 'fuzzyimplementation' of C:\Users\Rachael\IdeaProjects\indigoItsumadeC19I2 with conflicts.

--- src/edu/wpi/cs3733c19/teamI/Controllers2/ResultsController.java
+++ src/edu/wpi/cs3733c19/teamI/Controllers2/ResultsController.java
@@ -8,8 +8,10 @@ import com.jfoenix.controls.JFXButton;
 import edu.wpi.cs3733c19.teamI.Controllers2.dbUtilities.ReturnedValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableView;
+import javafx.scene.image.ImageView;
 
 import java.net.URL;
 import java.util.ArrayList;
@@ -26,14 +28,10 @@ public class ResultsController implements Initializable {
   //  @FXML
    // JFXTreeTableView
 
-    @FXML
-    TableView<JFXButton> tableView;
 
     public void setUp() {
       //  for (int i = 0; i < 4; i++) listView.getItems().add(new Label("Item " + i + " DRANK " + i));
       //  listView.getStyleClass().add("mylistview");
-        for (int i = 0; i < 4; i++) tableView.getItems().add(new JFXButton("Item " + i + " DRANK " + i));
-
     }
 
 
@@ -45,6 +43,19 @@ public class ResultsController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        for(int i=0;i<50;i++)
+        {
+
+
+            Label label = new Label("DESCRIPTION" + i);
+
+            label.setGraphic(new ImageView("edu/wpi/cs3733c19/teamI/Assets/placeholder_icon.png"));
+
+            //listView.getItems().add(label);
+
+
+
+        }
 
     }
 

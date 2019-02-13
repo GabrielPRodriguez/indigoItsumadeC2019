d7338f764b8c702fb17d4232156c3e52ab60e79d
Michael Antonelli <mmantonelli@wpi.edu>
2019-02-13 07:01:50 -0500

    currently the form checker mostly works

--- src/edu/wpi/cs3733c19/teamI/Controllers2/AgentWorkflowController.java
+++ src/edu/wpi/cs3733c19/teamI/Controllers2/AgentWorkflowController.java
@@ -62,7 +62,7 @@ public class AgentWorkflowController {
 
 
     @FXML
-    JFXButton submit_button;
+    JFXButton accept_button;
 
     @FXML
     JFXButton reject_button;
@@ -77,13 +77,61 @@ public class AgentWorkflowController {
     JFXButton specialist_button;
 
     @FXML
-    JFXButton choose_button1;
+    Button choose_button1;
 
     @FXML
-    JFXButton choose_button2;
+    Button choose_button2;
 
     @FXML
-    JFXButton choose_button3;
+    Button choose_button3;
+
+    @FXML
+    Button choose_button4;
+
+    @FXML
+    Button choose_button5;
+
+    @FXML
+    Button choose_button6;
+
+    @FXML
+    Button choose_button7;
+
+    @FXML
+    Button choose_button8;
+
+    @FXML
+    Button choose_button9;
+
+    @FXML
+    Label formID_1;
+
+    @FXML
+    Label formID_2;
+
+    @FXML
+    Label formID_3;
+
+    @FXML
+    Label formID_4;
+
+    @FXML
+    Label formID_5;
+
+    @FXML
+    Label formID_6;
+
+    @FXML
+    Label formID_7;
+
+    @FXML
+    Label formID_8;
+
+    @FXML
+    Label formID_9;
+
+
+
 
 
     @FXML
@@ -91,18 +139,6 @@ public class AgentWorkflowController {
 
     @FXML
     Label theUserName;
-
-
-    String formID_1 = "1";
-
-    String formID_2 = "2";
-
-    String formID_3 = "3";
-
-    String formID_4 = "4";
-
-    String formID_5 = "5";
-
 
     @FXML
     JFXButton goHome;
@@ -242,6 +278,173 @@ public class AgentWorkflowController {
     @FXML
     ListView listView;
 
+    String formStatus_string;
+    int currentFormID = 0;
+
+    //pulls unread forms from the database to be selected
+
+    @FXML
+    private void pull_Forms() throws Exception{
+        formID_1.setText("");
+        formID_2.setText("");
+        formID_3.setText("");
+        formID_4.setText("");
+        formID_5.setText("");
+        formID_6.setText("");
+        formID_7.setText("");
+        formID_8.setText("");
+        formID_9.setText("");
+        choose_button1.setDisable(false);
+        choose_button2.setDisable(false);
+        choose_button3.setDisable(false);
+        choose_button4.setDisable(false);
+        choose_button5.setDisable(false);
+        choose_button6.setDisable(false);
+        choose_button7.setDisable(false);
+        choose_button8.setDisable(false);
+        choose_button9.setDisable(false);
+        SQLDriver driver = new SQLDriver();
+        ArrayList<HashMap<String, ReturnedValue>>filtered_results = new ArrayList<HashMap<String, ReturnedValue>>();
+        for (HashMap<String, ReturnedValue>result:driver.select_all("form_db_from_spreadsheet.db", "form_db")){
+            //if (result.get("status").to_string().equals("unread")){
+
+                filtered_results.add(result);
+            //}
+        }
+        HashMap<Integer, Label>test = new HashMap<Integer, Label>();
+        test.put(1, formID_1);
+        test.put(2, formID_2);
+        test.put(3, formID_3);
+        test.put(4, formID_4);
+        test.put(5, formID_5);
+        test.put(6, formID_6);
+        test.put(7, formID_7);
+        test.put(8, formID_8);
+        test.put(9, formID_9);
+        if (filtered_results.size() > 0){
+            for (int i = 1; i < 10; i++){
+                try{
+                    HashMap<String, ReturnedValue>_temp = filtered_results.get(i-1);
+                    //test.get(i).setText("Form "+_temp.get("formID").to_string().replace(".0", ""));
+                    test.get(i).setText(_temp.get("formID").to_string().replace(".0", ""));
+                }
+                catch(Exception e){
+                    //pass
+                }
+            }
+
+            if(formID_1.getText().equals("")){
+                choose_button1.setDisable(true);
+            }
+            if(formID_2.getText().equals("")){
+                choose_button2.setDisable(true);
+            }
+            if(formID_3.getText().equals("")){
+                choose_button3.setDisable(true);
+            }
+            if(formID_4.getText().equals("")){
+                choose_button4.setDisable(true);
+            }
+            if(formID_5.getText().equals("")){
+                choose_button5.setDisable(true);
+            }
+            if(formID_6.getText().equals("")){
+                choose_button6.setDisable(true);
+            }
+            if(formID_7.getText().equals("")){
+                choose_button7.setDisable(true);
+            }
+            if(formID_8.getText().equals("")){
+                choose_button8.setDisable(true);
+            }
+            if(formID_9.getText().equals("")){
+                choose_button9.setDisable(true);
+            }
+
+        }
+        else{
+            formID_1.setText("No forms to be approved");
+            choose_button1.setDisable(true);
+            choose_button2.setDisable(true); //buttons are disabled when there are no forms
+            choose_button3.setDisable(true);
+            choose_button4.setDisable(true);
+            choose_button5.setDisable(true); //buttons are disabled when there are no forms
+            choose_button6.setDisable(true);
+            choose_button7.setDisable(true);
+            choose_button8.setDisable(true); //buttons are disabled when there are no forms
+            choose_button9.setDisable(true);
+        }
+    }
+
+    @FXML
+    private void chooseButtonHandler(ActionEvent choose) throws  Exception {
+        accept_button.setDisable(false);
+        reject_button.setDisable(false);
+
+        //int formID = 0;
+        if (choose.getSource() == choose_button1) {
+            //System.out.println(formID_1.getText());
+            currentFormID = Integer.parseInt(formID_1.getText());
+
+        } else if (choose.getSource() == choose_button2) {
+            currentFormID = Integer.parseInt(formID_2.getText());
+
+        } else if (choose.getSource() == choose_button3) {
+            currentFormID = Integer.parseInt(formID_3.getText());
+
+        }else if (choose.getSource() == choose_button4) {
+            currentFormID = Integer.parseInt(formID_4.getText());
+
+        }else if (choose.getSource() == choose_button5) {
+            currentFormID = Integer.parseInt(formID_5.getText());
+
+        }else if (choose.getSource() == choose_button6) {
+            currentFormID = Integer.parseInt(formID_6.getText());
+
+        }else if (choose.getSource() == choose_button7) {
+            currentFormID = Integer.parseInt(formID_7.getText());
+
+        }else if (choose.getSource() == choose_button8) {
+            currentFormID = Integer.parseInt(formID_8.getText());
+
+        }else if (choose.getSource() == choose_button9) {
+            currentFormID = Integer.parseInt(formID_9.getText());
+
+        }
+
+        SQLDriver driver = new SQLDriver();
+        HashMap<String, ReturnedValue>result = driver.get_data_by_value("form_db", "form_db_from_spreadsheet.db", "formID", new DBValue<Integer>(currentFormID));
+
+
+        //setting the text for each field from values in the database
+
+        repID_text.setText(result.get("repID").to_string());
+        plantRegistry_text.setText(result.get("plantRegistry").to_string());
+        //omesticImported_text.setText(result.get("domesticOrImported").to_string());
+        serialNum_text.setText(result.get("serialNumber").to_string());
+        //productType_text.setText(result.get("beverageType").to_string());
+        brandName_text.setText(result.get("brandName").to_string());
+        fancifulName_text.setText(result.get("fancifulName").to_string());
+        //nameAddress_text.setText(result.get("nameAndAddress").to_string());
+        //mailingAddress_text.setText(result.get("mailingAddress").to_string());
+
+        System.out.println(result.toString());
+        streetAdress_text.setText(result.get("streetAddress").to_string());
+
+        formula_text.setText(result.get("formula").to_string());
+        grapeVarietal_text.setText(result.get("grapeVarietals").to_string());
+        wineAppellation_text.setText(result.get("wineAppellation").to_string());
+        winepH_text.setText(result.get("pHValue").to_string());
+        vintage_text.setText(result.get("vintage").to_string());
+        alcoholContent_text.setText(result.get("alcoholContent").to_string());
+        //phoneNum_text.setText(result.get("phoneNumber").to_string());
+        //emailAddress_text.setText(result.get("email").to_string());
+        brandedInfo_text.setText(result.get("extraInfo").to_string());
+        //applicationDate_text.setText(result.get("dateOfApplication").to_string());
+        applicantName_text.setText(result.get("nameOfApplicant").to_string());
+        formStatus_string = (result.get("status").to_string()); //I use two variables because I need the formStatus text as a string
+        //formStatus_text.setText(formStatus_string);
+    }
 
 /*
     @FXML
@@ -255,214 +458,4 @@ public class AgentWorkflowController {
 
 */
 
-    String formStatus_string;
-    int currentFormID = 0;
-
-    //pulls unread forms from the database to be selected
-
-    @FXML
-    private void pull_Forms() throws Exception{
-       // formID_1.setText("");
-        //formID_2.setText("");
-       // formID_3.setText("");
-        /*
-        choose_button1.setDisable(false);
-        choose_button2.setDisable(false);
-        choose_button3.setDisable(false);
-
-        */
-        //this.formID_column.setCellValueFactory(new PropertyValueFactory<sub_Form, String>("Form_ID"));
-
-        ObservableList<String> id = FXCollections.observableArrayList(formID_1, formID_2, formID_3, formID_4, formID_5);
-
-        //listView.
-        //workTable.setItems();
-        SQLDriver driver = new SQLDriver();
-        ArrayList<HashMap<String, ReturnedValue>> filtered_results = new ArrayList<HashMap<String, ReturnedValue>>();
-        for (HashMap<String, ReturnedValue>result:driver.select_all("form_data.db", "form_data")){
-            //if (result.get("status").to_string().equals("unread")){
-
-                filtered_results.add(result);
-            //}
-        }
-        HashMap<Integer, String>test = new HashMap<Integer, String>();
-
-        test.put(1, formID_1);
-        test.put(2, formID_2);
-        test.put(3, formID_3);
-        if (filtered_results.size() > 0){
-            for (int i = 1; i < 4; i++){
-                try{
-                    HashMap<String, ReturnedValue>_temp = filtered_results.get(i-1);
-                    //test.get(i).setText("Form "+_temp.get("formID").to_string().replace(".0", ""));
-                    test.replace(i, (_temp.get("formID").to_string().replace(".0", "")));
-                    listView.setItems(id);
-
-                }
-                catch(Exception e){
-                    //pass
-                }
-            }
-
-            /*
-            if(formID_1.getText().equals("")){
-                choose_button1.setDisable(true);
-            }
-            if(formID_2.getText().equals("")){
-                choose_button2.setDisable(true);
-            }
-            if(formID_3.getText().equals("")){
-                choose_button3.setDisable(true);
-            }
-            */
-
-        }
-        else{
-            /*
-            formID_1.setText("No forms to be approved");
-            choose_button1.setDisable(true);
-            choose_button2.setDisable(true); //buttons are disabled when there are no forms
-            choose_button3.setDisable(true);
-            */
-        }
-    }
-
-
-
-    //runs when any of the choose buttons are clicked. The text fields are filled with information from the form
-    //associated with the button clicked
-    @FXML
-    private void chooseButtonHandler()throws  Exception{
-
-        System.out.println(listView.getSelectionModel().getSelectedItems().toString());
-        currentFormID = Integer.parseInt(listView.getSelectionModel().getSelectedItems().toString());
-        //submit_button.setDisable(false);
-       // reject_button.setDisable(false);
-
-        //int formID = 0;
-        /*
-        if(choose.getSource() == choose_button1){
-            //System.out.println(formID_1.getText());
-            currentFormID = Integer.parseInt(formID_1.getText());
-
-        }
-
-        else if(choose.getSource() == choose_button2){
-            currentFormID = Integer.parseInt(formID_2.getText());
-
-        }
-
-        else if(choose.getSource() == choose_button3){
-            currentFormID = Integer.parseInt(formID_3.getText());
-
-        }
-        */
-
-
-
-
-
-        SQLDriver driver = new SQLDriver();
-        HashMap<String, ReturnedValue>result = driver.get_data_by_value("form_data", "form_data.db", "formID", new DBValue<Integer>(currentFormID));
-
-
-        //setting the text for each field from values in the database
-
-        repID_text.setText(result.get("repID").to_string());
-        plantRegistry_text.setText(result.get("plantRegistry").to_string());
-        //domesticImported_text.setText(result.get("domesticOrImported").to_string());
-        serialNum_text.setText(result.get("serialNumber").to_string());
-       // productType_text.setText(result.get("beverageType").to_string());
-        brandName_text.setText(result.get("brandName").to_string());
-        fancifulName_text.setText(result.get("fancifulName").to_string());
-        //nameAddress_text.setText(result.get("nameAndAddress").to_string());
-       // mailingAddress_text.setText(result.get("mailingAddress").to_string());
-        formula_text.setText(result.get("formula").to_string());
-        grapeVarietal_text.setText(result.get("grapeVarietals").to_string());
-        wineAppellation_text.setText(result.get("wineAppellation").to_string());
-        winepH_text.setText(result.get("pHValue").to_string());
-        vintage_text.setText(result.get("vintage").to_string());
-        alcoholContent_text.setText(result.get("alcoholContent").to_string());
-        phoneNum_text.setText(result.get("phoneNumber").to_string());
-        emailAddress_text.setText(result.get("email").to_string());
-        brandedInfo_text.setText(result.get("extraInfo").to_string());
-        applicationDate_text.setText(result.get("dateOfApplication").to_string());
-        applicantName_text.setText(result.get("nameOfApplicant").to_string());
-       // formStatus_string = (result.get("status").to_string()); //I use two variables because I need the formStatus text as a string
-      //  formStatus_text.setText(formStatus_string);
-
-
-
-
-    }
-
-    //runs when the submit button is clicked, and sets formstatus to approved.
-
-    @FXML
-    private void approveHandler() throws IOException, Exception{
-
-        formStatus_string = "approved";
-
-        SQLDriver.setApprovalStatus(currentFormID, formStatus_string);
-        Date date = new Date();
-        String theDate = date.toString();
-        date.equals(date.getTime()+10000);
-        String exDate = date.toString();
-        SQLDriver.setApprovalDate(currentFormID, theDate);
-        SQLDriver.setApprovingUser(currentFormID, this.theUserName.getText());
-        SQLDriver.setExpirationDate(currentFormID, exDate);
-
-
-
-        clearFields();
-        pull_Forms();
-
-        submit_button.setDisable(true);
-        reject_button.setDisable(true);
-
-    }
-
-
-    //code run when the reject button is pressed, sets the status of the form in the database to "reject"
-    @FXML
-    private void rejectHandler() throws IOException, Exception{
-
-        formStatus_string = "reject";
-
-        SQLDriver.setApprovalStatus(currentFormID, formStatus_string);
-        clearFields();
-
-        pull_Forms();
-
-        submit_button.setDisable(true); //submit and reject buttons cannot be pressed until another form is pulled
-        reject_button.setDisable(true);
-
-    }
-
-    //clears all of the displayed fields
-
-    private void clearFields(){
-        repID_text.clear();
-        plantRegistry_text.clear();
-        applicantName_text.clear();
-        applicationDate_text.clear();
-        brandedInfo_text.clear();
-        emailAddress_text.clear();
-        phoneNum_text.clear();
-        alcoholContent_text.clear();
-        vintage_text.clear();
-        winepH_text.clear();
-        grapeVarietal_text.clear();
-        wineAppellation_text.clear();
-        formula_text.clear();
-        mailingAddress_text.clear();
-        fancifulName_text.clear();
-        brandName_text.clear();
-        productType_text.clear();
-        serialNum_text.clear();
-       // domesticImported_text.clear();
-       // nameAddress_text.clear();
-        formStatus_text.clear();
-
-    }
 }

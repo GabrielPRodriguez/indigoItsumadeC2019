2eba1c102fcd266321a8876e2a87d515e314c556
Rasheeda Samih <rsamih@wpi.edu>
2019-02-12 19:44:09 -0500

    Reformatted the about page, added information

--- src/edu/wpi/cs3733c19/teamI/Controllers2/ResultsController.java
+++ src/edu/wpi/cs3733c19/teamI/Controllers2/ResultsController.java
@@ -1,14 +1,11 @@
 package edu.wpi.cs3733c19.teamI.Controllers2;
 
-import com.jfoenix.controls.JFXListView;
-import com.jfoenix.controls.JFXListView;
+import com.jfoenix.controls.JFXButton;
 import edu.wpi.cs3733c19.teamI.Controllers2.dbUtilities.ReturnedValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
-import javafx.scene.image.ImageView;
+import javafx.scene.control.TableView;
 
 import java.net.URL;
 import java.util.ArrayList;
@@ -28,11 +25,11 @@ public class ResultsController implements Initializable {
     }
 
     @FXML
-    JFXListView<Label> listView;
+    TableView<JFXButton> tableView;
 
     public void setUp() {
-        for (int i = 0; i < 4; i++) listView.getItems().add(new Label("Item " + i + " DRANK " + i));
-        listView.getStyleClass().add("mylistview");
+        for (int i = 0; i < 4; i++) tableView.getItems().add(new JFXButton("Item " + i + " DRANK " + i));
+
     }
 
 
@@ -44,20 +41,7 @@ public class ResultsController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        for(int i=0;i<50;i++)
-        {
 
-
-            Label label = new Label("DESCRIPTION" + i);
-
-            label.setGraphic(new ImageView("edu/wpi/cs3733c19/teamI/Assets/placeholder_icon.png"));
-
-            //listView.getItems().add(label);
-
-            System.out.println(testString);
-
-            System.out.println("Transfered:" + resultsList);
-        }
     }
 
 

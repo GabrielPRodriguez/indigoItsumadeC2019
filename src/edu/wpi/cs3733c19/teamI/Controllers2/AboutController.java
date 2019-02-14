d7c1098c0a017597410e7678bd5d843c79cdf335
YossefN <yhnaim@wpi.edu>
2019-02-13 23:09:36 -0500

    Started on the revamp of the team's About page => added all necessary fields/ areas for face shots and descriptions => added two descriptions
    - Yossef

--- src/edu/wpi/cs3733c19/teamI/Controllers2/AboutController.java
+++ src/edu/wpi/cs3733c19/teamI/Controllers2/AboutController.java
@@ -5,7 +5,11 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Circle;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -43,10 +47,16 @@ public class AboutController implements Initializable {
         tb_loginButton.setText("logout");
     }
 
+    @FXML
+    Circle yossef;
+
     @Override
-    public void initialize(URL location, ResourceBundle resources) {
-
-
+    public void initialize(URL location, ResourceBundle resources)
+    {
+//        Image image = new Image(new File("Assets/blue_pill_button.png").toURI().toString());
+//        ImagePattern imagePattern = new ImagePattern(image);
+//
+//        yossef.setFill(imagePattern);
     }
 
     @FXML

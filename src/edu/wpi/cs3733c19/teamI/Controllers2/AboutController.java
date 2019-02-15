a7b3c48b0ef78ebd7dde4444d8f8ed97e2572855
Rasheeda Samih <rsamih@wpi.edu>
2019-02-15 17:41:33 -0500

    Tryna fix ui

--- src/edu/wpi/cs3733c19/teamI/Controllers2/AboutController.java
+++ src/edu/wpi/cs3733c19/teamI/Controllers2/AboutController.java
@@ -1,8 +1,6 @@
 package edu.wpi.cs3733c19.teamI.Controllers2;
 
-import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.shape.Circle;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -11,16 +9,10 @@ public class AboutController implements Initializable {
 
 
 
-    @FXML
-    Circle yossef;
-
     @Override
     public void initialize(URL location, ResourceBundle resources)
     {
-//        Image image = new Image(new File("Assets/blue_pill_button.png").toURI().toString());
-//        ImagePattern imagePattern = new ImagePattern(image);
-//
-//        yossef.setFill(imagePattern);
+        
     }
 
 

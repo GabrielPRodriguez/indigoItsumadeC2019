f422c912b83b0b851abe7c4c4139998defe75e5a
Rachael <resallie@wpi.edu>
2019-02-26 14:51:10 -0500

    Convert to SQL

--- src/edu/wpi/cs3733c19/teamI/Controllers2/FormSubmissionController.java
+++ src/edu/wpi/cs3733c19/teamI/Controllers2/FormSubmissionController.java
@@ -481,9 +481,10 @@ public class FormSubmissionController implements Initializable {
                     data.currentFormID = "";
                 }
                 */
+                SQLDriver driver = new SQLDriver();
                 //sets the names of columns in the database, if additional form fields are added, please add a new column
                 String [] columns = {"formID", "repID", "plantRegistry", "domesticOrImported", "serialNumber", "brandName", "beverageType", "fancifulName", "permitName", "streetAddress","city","state", "zip", "extraInfo", "dateOfApplication", "formula", "grapeVarietals", "vintage", "wineAppellation", "email", "phoneNumber", "pHValue", "alcoholContent", "status", "approvingUser", "approvalDate", "expirationDate", "issuedDate", "volume", "appType", "surrenderDate", "qualifier", "name"};
-                MongoDriver driver = new MongoDriver("mongodb+srv://firstuser1:newTestCred@cs3733-hgmot.mongodb.net/test?retryWrites=true", columns);
+                //MongoDriver driver = new MongoDriver("mongodb+srv://firstuser1:newTestCred@cs3733-hgmot.mongodb.net/test?retryWrites=true", columns);
                 //
                 //contains the datatype of each column in the database, when adding a new column, please also add it's datatype here/
                 //"text" for strings and "real" for doubles/integers
